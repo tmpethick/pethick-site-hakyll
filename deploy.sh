736785cf652e73fed1b3533c18f526deb43705f2
Thomas Pethick <tmpethick@gmail.com>
2022-02-20 22:27:04 +0100

    Fixes deployment

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,10 @@ stack exec site build
 
 # Get previous files
 git fetch --all
-git checkout -b master --track origin/master
+git checkout master
 
 # Overwrite existing files with new files
-cp -a _site/. .
+yes | cp -a _site/. .
 
 # Commit
 git add -A
@@ -25,5 +25,4 @@ git push origin master:master
 
 # Restoration
 git checkout develop
-git branch -D master
 git stash pop
