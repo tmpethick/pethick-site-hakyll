24bd2afd7ed033952430c3d08cc1877e95b1194b
Thomas Pethick <tmpethick@gmail.com>
2022-02-20 22:25:29 +0100

    Publish.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack exec site clean
+stack exec site build
+
+# Get previous files
+git fetch --all
+git checkout master
+
+# Overwrite existing files with new files
+yes | cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
